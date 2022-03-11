5944c2e5b6df9c6306958c58225e59f4fea293f3
Berenguer Pou <berenguer.pou@gmail.com>
2022-03-11 09:43:41 +0100

    add deleteTrack controller

--- src/server/controllers/tracksControllers.js
+++ src/server/controllers/tracksControllers.js
@@ -9,6 +9,25 @@ const getAllTracks = async (req, res, next) => {
   }
 };
 
+const deleteTrack = async (res, req, next) => {
+  const { id } = req.params;
+  try {
+    const deletedTrack = await Track.findByIdAndDelete(id);
+    if (deletedTrack) {
+      res.json(deletedTrack.id);
+      return;
+    }
+    const error = new Error("ID not found");
+    error.code = 404;
+    next(error);
+  } catch (error) {
+    error.message = "Bad request triying to delete track";
+    error.code = 400;
+    next(error);
+  }
+};
+
 module.exports = {
   getAllTracks,
+  deleteTrack,
 };
