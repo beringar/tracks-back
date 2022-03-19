78abed3a1e2705797264b4a5d9c07698b2dc5c53
Berenguer Pou <berenguer.pou@gmail.com>
2022-03-19 10:39:11 +0100

    refactored getAllTracks controller, sort to be in front

--- src/db/models/Track.js
+++ src/db/models/Track.js
@@ -1,33 +1,36 @@
 const { model, Schema } = require("mongoose");
 const trackDifficultyEnum = require("./types/trackDifficultyEnum");
 
-const TrackSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const TrackSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    refuge: {
+      type: String,
+      required: true,
+    },
+    difficulty: {
+      type: String,
+      enum: Object.values(trackDifficultyEnum),
+      required: true,
+    },
+    kids: { type: Boolean, required: true },
+    seasons: [{ type: String, required: true }],
+    description: { type: String, required: true },
+    image: {
+      type: String,
+      required: true,
+    },
+    gpx: {
+      type: String,
+      required: true,
+    },
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
   },
-  refuge: {
-    type: String,
-    required: true,
-  },
-  difficulty: {
-    type: String,
-    enum: Object.values(trackDifficultyEnum),
-    required: true,
-  },
-  kids: { type: Boolean, required: true },
-  seasons: [{ type: String, required: true }],
-  description: { type: String, required: true },
-  image: {
-    type: String,
-    required: true,
-  },
-  gpx: {
-    type: String,
-    required: true,
-  },
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-});
+  { timestamps: true }
+);
 
 const Track = model("Track", TrackSchema, "tracks");
 
